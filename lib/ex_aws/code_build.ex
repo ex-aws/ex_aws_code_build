04523e9e15a69ce97f99d61f692a370a0f666ac9
Dave Persing <persing@adobe.com>
2018-02-09 22:00:21 -0800

    Tests + create/update/delete

--- lib/ex_aws/code_build.ex
+++ lib/ex_aws/code_build.ex
@@ -1,15 +1,15 @@
 defmodule ExAws.CodeBuild do
   @moduledoc """
-  AWS CodeBuild Service.
-
-  Uses JSON protocol.
-
+  AWS CodeBuild Service for ExAws
   """
-  import ExAws.Utils
+
+  import ExAws.Utils, only: [camelize_keys: 1]
 
   @version "2016-10-06"
+  @namespace "CodeBuild_20161006"
 
-  @spec batch_delete_builds([binary()]) :: ExAws.Operation.JSON.t()
+  @doc "Batch delete builds"
+  @spec batch_delete_builds(ids :: [binary()]) :: ExAws.Operation.JSON.t()
   def batch_delete_builds(ids) do
     query_params = %{
       "Action" => "BatchDeleteBuilds",
@@ -20,8 +20,12 @@ defmodule ExAws.CodeBuild do
     request(:batch_delete_builds, query_params)
   end
 
-  @spec batch_get_builds([binary()]) :: ExAws.Operation.JSON.t()
-  def batch_get_builds(ids) do
+  @doc "Batch get builds"
+  @spec batch_get_builds(ids :: [binary()]) :: ExAws.Operation.JSON.t()
+  def batch_get_builds(ids) when is_binary(ids) do
+    batch_get_builds([ids])
+  end
+  def batch_get_builds(ids) when is_list(ids) do
     query_params = %{
       "Action" => "BatchGetBuilds",
       "Version" => @version,
@@ -31,7 +35,8 @@ defmodule ExAws.CodeBuild do
     request(:batch_get_builds, query_params)
   end
 
-  @spec batch_get_projects([binary()]) :: ExAws.Operation.JSON.t()
+  @doc "Batch get projects"
+  @spec batch_get_projects(names :: [binary()]) :: ExAws.Operation.JSON.t()
   def batch_get_projects(names) do
     query_params = %{
       "Action" => "BatchGetProjects",
@@ -42,24 +47,81 @@ defmodule ExAws.CodeBuild do
     request(:batch_get_projects, query_params)
   end
 
-  def create_project do
+  @type create_project_opt :: [
+    {:badge_enabled, boolean()} |
+    {:cache, map()} |
+    {:encryption_key, binary()} |
+    {:service_role, binary()} |
+    {:tags, [map()]} |
+    {:timeout_in_minutes, pos_integer()} |
+    {:vpc_config, map()}
+  ]
 
+  @doc "Create a new CodeBuild project"
+  @spec create_project(
+    artifacts :: map(),
+    environment :: map(),
+    name :: binary(),
+    source :: map()) :: ExAws.Operation.JSON.t()
+  @spec create_project(
+    artifacts :: map(),
+    environment :: map(),
+    name :: binary(),
+    source :: map(),
+    opts :: create_project_opt()) :: ExAws.Operation.JSON.t()
+  def create_project(artifacts, environment, name, source, opts \\ []) do
+    query_params = opts
+    |> normalize_opts()
+    |> Map.merge(%{
+      "Action" => "CreateProject",
+      "Version" => @version,
+      "artifacts" => normalize_opts(artifacts),
+      "environment" => normalize_opts(environment),
+      "name" => name,
+      "source" => normalize_opts(source)})
+
+    request(:create_project, query_params)
   end
 
-  def create_webhook do
+  @doc "Create webhook"
+  @spec create_webhook(project_name :: binary()) :: ExAws.Operation.JSON.t()
+  def create_webhook(project_name) do
+    query_params = %{
+      "Action" => "CreateWebhook",
+      "Version" => @version,
+      "projectName" => project_name
+    }
 
+    request(:create_webhook, query_params)
   end
 
-  def delete_project do
+  @doc "Delete project"
+  @spec delete_project(project_name :: binary()) :: ExAws.Operation.JSON.t()
+  def delete_project(project_name) do
+    query_params = %{
+      "Action" => "DeleteProject",
+      "Version" => @version,
+      "projectName" => project_name
+    }
 
+    request(:delete_project, query_params)
   end
 
-  def delete_webhook do
+  @doc "Delete webhook"
+  @spec delete_webhook(project_name :: binary()) :: ExAws.Operation.JSON.t()
+  def delete_webhook(project_name) do
+    query_params = %{
+      "Action" => "DeleteWebhook",
+      "Version" => @version,
+      "projectName" => project_name
+    }
 
+    request(:delete_webhook, query_params)
   end
 
+  @doc "List builds"
   @spec list_builds() :: ExAws.Operation.JSON.t()
-  @spec list_builds(Keyword.t) :: ExAws.Operation.JSON.t()
+  @spec list_builds(opts :: Keyword.t()) :: ExAws.Operation.JSON.t()
   def list_builds(opts \\ []) do
     query_params = opts
     |> normalize_opts()
@@ -70,8 +132,9 @@ defmodule ExAws.CodeBuild do
     request(:list_builds, query_params)
   end
 
-  @spec list_builds_for_project(binary()) :: ExAws.Operation.JSON.t()
-  @spec list_builds_for_project(binary(), Keyword.t()) :: ExAws.Operation.JSON.t()
+  @doc "List builds for project"
+  @spec list_builds_for_project(project_name :: binary()) :: ExAws.Operation.JSON.t()
+  @spec list_builds_for_project(project_name :: binary(), Keyword.t()) :: ExAws.Operation.JSON.t()
   def list_builds_for_project(project_name, opts \\ []) do
     query_params = opts
     |> normalize_opts()
@@ -83,6 +146,7 @@ defmodule ExAws.CodeBuild do
     request(:list_builds_for_project, query_params)
   end
 
+  @doc "List curated environment images"
   @spec list_curated_environment_images() :: ExAws.Operation.JSON.t()
   def list_curated_environment_images do
     query_params = %{
@@ -95,7 +159,7 @@ defmodule ExAws.CodeBuild do
 
   @doc "List projects"
   @spec list_projects() :: ExAws.Operation.JSON.t()
-  @spec list_projects(Keyword.t()) :: ExAws.Operation.JSON.t()
+  @spec list_projects(opts :: Keyword.t()) :: ExAws.Operation.JSON.t()
   def list_projects(opts \\ []) do
     query_params = opts
     |> normalize_opts()
@@ -106,7 +170,8 @@ defmodule ExAws.CodeBuild do
     request(:list_projects, query_params)
   end
 
-  @spec start_build(binary(), Keyword.t()) :: ExAws.Operation.JSON.t()
+  @doc "Start build"
+  @spec start_build(project_name :: binary(), opts :: Keyword.t()) :: ExAws.Operation.JSON.t()
   def start_build(project_name, opts \\ []) do
     query_params = opts
     |> normalize_opts()
@@ -118,7 +183,8 @@ defmodule ExAws.CodeBuild do
     request(:start_build, query_params)
   end
 
-  @spec stop_build(binary()) :: ExAws.Operation.JSON.t()
+  @doc "Stop build"
+  @spec stop_build(id :: binary()) :: ExAws.Operation.JSON.t()
   def stop_build(id) do
     query_params = %{
       "Action" => "StopBuild",
@@ -129,8 +195,34 @@ defmodule ExAws.CodeBuild do
     request(:stop_build, query_params)
   end
 
-  def update_project do
+  @type update_project_opts :: [
+    {:artifacts, map()} |
+    {:badge_enabled, boolean()} |
+    {:cache, map()} |
+    {:description, binary()} |
+    {:encryption_key, binary()} |
+    {:environment, map()} |
+    {:name, binary()} |
+    {:service_role, binary()} |
+    {:source, map()} |
+    {:tags, [map()]} |
+    {:timeout_in_minutes, pos_integer()} |
+    {:vpc_config, map()}
+  ]
 
+  @doc "Update project"
+  @spec update_project(project_name :: binary()) :: ExAws.Operation.JSON.t()
+  @spec update_project(project_name :: binary(), opts :: update_project_opts()) :: ExAws.Operation.JSON.t()
+  def update_project(project_name, opts \\ []) do
+    query_params = opts
+    |> normalize_opts()
+    |> Map.merge(%{
+      "Action" => "UpdateProject",
+      "Version" => @version,
+      "name" => project_name
+      })
+
+    request(:update_project, query_params)
   end
 
   defp request(action, params, opts \\ %{}) do
@@ -142,15 +234,26 @@ defmodule ExAws.CodeBuild do
     ExAws.Operation.JSON.new(:codebuild, %{
           data: params,
           headers: [
-            {"x-amz-target", "CodeBuild_20161006.#{operation}"},
-            {"content-type", "application/x-amz-json-1.1"},
+            {"x-amz-target", "#{@namespace}.#{operation}"},
+            {"content-type", "application/x-amz-json-1.1"}
           ]
     } |> Map.merge(opts))
   end
 
+  # This JSON API requires lower camelCased parameters.
   defp normalize_opts(opts) do
     opts
     |> Enum.into(%{})
     |> camelize_keys()
+    |> Enum.reduce(%{},
+      fn({k, v}, acc) ->
+        Map.put(acc, to_lower_camel_case(k), v)
+      end)
+  end
+
+  defp to_lower_camel_case(string) do
+    {first, rest} = String.split_at(string, 1)
+
+    String.downcase(first) <> rest
   end
 end
--- test/ex_aws_code_build_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule ExAws.CodeBuildTest do
-  use ExUnit.Case
-  doctest ExAws.CodeBuild
-
-  test "greets the world" do
-    assert ExAws.CodeBuild.hello() == :world
-  end
-end
